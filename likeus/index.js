136b97a2f5620e9f0ee6f6ed17c8c6032c7449e3
Alex <alexserban77@gmail.com>
2025-01-25 17:17:14 -0500

    integration

--- likeus/index.js
+++ likeus/index.js
@@ -1,58 +1,70 @@
-import express from "express";
-import dotenv from "dotenv";
-import OpenAI from "openai";
-import pdfUtil from 'pdf-to-text';
+import express from 'express'
+import dotenv from 'dotenv'
+import OpenAI from 'openai'
+import pdfUtil from 'pdf-to-text'
+import multer from 'multer'
+import cors from 'cors'
+import fs from 'fs'
 
 // initialize dotenv to load env vars
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const app = express()
+const PORT = process.env.PORT || 5001
 
 const openai = new OpenAI({
   organization: process.env.ORGANIZATION,
   project: process.env.PROJECT,
-  apiKey: process.env.KEY
-});
+  apiKey: process.env.KEY,
+})
 
 // middleware to parse incoming json requests
-app.use(express.json());
-
+app.use(express.json())
+app.use(cors())
+app.use(multer().any())
+const upload = multer()
 // basic route for testing
-app.get("/", (req, res) => {
-  res.send("we're up");
-});
+app.get('/', (req, res) => {
+  res.send("we're up")
+})
 
 // example api route
-app.get("/api/example", (req, res) => {
-  res.json({ message: "example endpoint" });
-});
+app.get('/api/example', (req, res) => {
+  res.json({ message: 'example endpoint' })
+})
+
+// // TODO step 1 & 2 & 3
+// const dataBuffer = fs.readFileSync("./sample.pdf");
+
+// pdfParse(dataBuffer).then((data) => {
+//   console.log("PDF Text Content:");
+//   console.log(data.text);
+// });
 
 // start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
 
-function pdfText(file){
+function pdfText(file) {
   return new Promise((resolve, reject) => {
-    pdfUtil.pdfToText(file, function(err, data) {
+    pdfUtil.pdfToText(file, function (err, data) {
       if (err) {
-        reject(err);
+        reject(err)
       } else {
-        resolve(data);
+        resolve(data)
       }
     })
   })
 }
 
-
 async function callGPT(subject, difficulty, rubric, submission, outline) {
-    const stream = await openai.chat.completions.create({
-        model: "gpt-4o-mini",
-        messages: [{ 
-            role: "user", 
-            content: 
-            `You are an expert in ${subject} and are known as a ${difficulty} marker. You are grading the assignment and rubric that will be provided. What grade would you provide the submission based off of the rubric? Please provide only a numeric grade for this section and nothing else.
+  const stream = await openai.chat.completions.create({
+    model: 'gpt-4o-mini',
+    messages: [
+      {
+        role: 'user',
+        content: `You are an expert in ${subject} and are known as a ${difficulty} marker. You are grading the assignment and rubric that will be provided. What grade would you provide the submission based off of the rubric? Please provide only a numeric grade for this section and nothing else.
             Provide a brief explanation as to which level is best met for each criteria along with the numeric grade you would give.
             If there is any criteria that does not meet the maximum level, provide feedback on how to improve. Be specific on where in the submission, and what could be improved, but do not write any new content for the student as we do not want to violate any academic integrity rules. Do not be afraid to critique the work.
             Make sure to separate the rubric grade/explanation and the submission feedback in two separate sections.
@@ -60,31 +72,37 @@ async function callGPT(subject, difficulty, rubric, submission, outline) {
             Here is the submission text: ${submission}.
             Here is the rubric: ${rubric}.
             `,
-            // role: "user", content: "Hi"
-        }],
-        store: true,
-        stream: true,
-    });
-    return stream
+        // role: "user", content: "Hi"
+      },
+    ],
+    store: true,
+    stream: true,
+  })
+  return stream
 }
 
-
 app.post('/api/intro', async (req, res) => {
-    try{
-      let subject, difficulty, rubric, submission, outline = req.body
-      const parsed_rubric = await pdfText(rubric)
-      const parsed_submission = await pdfText(submission)
-      const parsed_outline = await pdfText(outline)
-      const stream = await callGPT(subject, difficulty, parsed_rubric, parsed_submission, parsed_outline);
-      for await (const chunk of stream) {
-        process.stdout.write(chunk.choices[0]?.delta?.content || "");
+  try {
+    let subject,
+      difficulty = req.body
+    const parsed_rubric = await pdfText(req.files[2])
+    const parsed_submission = await pdfText(req.files[0])
+    const parsed_outline = await pdfText(req.files[1])
+    const stream = await callGPT(
+      subject,
+      difficulty,
+      parsed_rubric,
+      parsed_submission,
+      parsed_outline
+    )
+    for await (const chunk of stream) {
+      process.stdout.write(chunk.choices[0]?.delta?.content || '')
     }
     return res.json({
-      message: "Good shit"
+      message: 'Good shit',
     })
-  }
-  catch(error) {
-    return res.json({message: "CMON MAN", error_message: error})
+  } catch (error) {
+    return res.json({ message: 'CMON MAN', error_message: error })
   }
 })
 
--- notlikeus/src/pages/Home.js
+++ notlikeus/src/pages/Home.js
@@ -1,25 +1,78 @@
 import '../css/Home.css'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
-  const [selectedFiles, setSelectedFiles] = useState({
-    file1: null,
-    file2: null,
-    file3: null,
+  const navigate = useNavigate()
+  const [files, setFiles] = useState({
+    assignment: null,
+    outline: null,
+    rubric: null,
   })
+  const [subject, setSubject] = useState('Business')
+  const [difficulty, setDifficulty] = useState(1)
 
   const handleFileChange = (event) => {
     const { id, files } = event.target
-    setSelectedFiles((prevFiles) => ({
+    setFiles((prevFiles) => ({
       ...prevFiles,
       [id]: files[0] || null, // Store the first selected file or null
     }))
   }
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    // Send the selected files to the server
-    console.log(selectedFiles)
+  //   console.log(file1Element)
+  //   file1Element.addEventListener('change', (event) => {
+  //     const files = event.target.files // this gives you a filelist object
+  //     if (files.length > 0) {
+  //       const file = files[0] // access the first file (if needed)
+  //       console.log(file) // debug: check the file object
+  //       // process/save the file as needed
+  //     }
+  //   })
+
+  const handleDifficultyChange = (e) => {
+    setDifficulty(e.target.value)
+  }
+
+  const handleSubjectChange = (e) => {
+    setSubject(e.target.value)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const formData = new FormData()
+
+    // append files to the formdata object
+    formData.append('assignment', files.assignment)
+    formData.append('outline', files.outline)
+    formData.append('rubric', files.rubric)
+    if (difficulty == 1) {
+      formData.append('difficulty', 'chill')
+    } else if (difficulty == 2) {
+      formData.append('difficulty', 'fair')
+    } else if (difficulty == 3) {
+      formData.append('difficulty', 'harsh')
+    }
+    formData.append('subject', subject)
+
+    console.log(formData)
+
+    try {
+      const response = await fetch('http://localhost:5001/api/intro', {
+        method: 'POST',
+        body: formData,
+      })
+
+      if (response.ok) {
+        console.log('Files uploaded successfully!')
+        navigate('/output')
+      } else {
+        console.error('File upload failed.')
+      }
+    } catch (error) {
+      console.error('Error while uploading files:', error)
+    }
   }
 
   return (
@@ -31,60 +84,69 @@ function Home() {
       </div>
       <form className="container" onSubmit={handleSubmit}>
         <div className="subheader">Subject</div>
-        <select className="dropDown">
-          <option value="option1">Business</option>
-          <option value="option2">English</option>
-          <option value="option3">Econ</option>
+        <select className="dropDown" onChange={handleSubjectChange}>
+          <option value="Business">Business</option>
+          <option value="English">English</option>
+          <option value="Econ">Econ</option>
         </select>
 
         <div className="subheader">Assignment</div>
         <div className="fileUploadContainer">
-          <label className="fileUploadLabel" htmlFor="file1">
-            {selectedFiles.file1 ? selectedFiles.file1.name : 'Choose File'}
+          <label className="fileUploadLabel" htmlFor="assignment">
+            {files.assignment ? files.assignment.name : 'Choose File'}
           </label>
           <input
             type="file"
-            id="file1"
+            id="assignment"
             className="fileUpload"
             onChange={handleFileChange}
           />
 
           <div className="subheader">Assignment Instructions</div>
-          <label className="fileUploadLabel" htmlFor="file2">
-            {selectedFiles.file2 ? selectedFiles.file2.name : 'Choose File'}
+          <label className="fileUploadLabel" htmlFor="outline">
+            {files.outline ? files.outline.name : 'Choose File'}
           </label>
           <input
             type="file"
-            id="file2"
+            id="outline"
             className="fileUpload"
             onChange={handleFileChange}
           />
 
           <div className="subheader">Assignment Rubric</div>
-          <label className="fileUploadLabel" htmlFor="file3">
-            {selectedFiles.file3 ? selectedFiles.file3.name : 'Choose File'}
+          <label className="fileUploadLabel" htmlFor="rubric">
+            {files.rubric ? files.rubric.name : 'Choose File'}
           </label>
           <input
             type="file"
-            id="file3"
+            id="rubric"
             className="fileUpload"
             onChange={handleFileChange}
           />
         </div>
-        <div className="subheader">Grading Difficulty</div>
-        <div className="sliderContainer">
-          <div className="slider">
-            <input type="range" min="1" max="3" step="1" defaultValue="1" />
-            <div className="sliderLabels">
-              <span>Chill</span>
-              <span>Fair</span>
-              <span>Harsh</span>
-            </div>
+      </form>
+      <div className="subheader">Grading Difficulty</div>
+      <div className="sliderContainer">
+        <div className="slider">
+          <input
+            type="range"
+            min="1"
+            max="3"
+            step="1"
+            defaultValue="1"
+            onChange={handleDifficultyChange}
+          />
+          <div className="sliderLabels">
+            <span>Chill</span>
+            <span>Fair</span>
+            <span>Harsh</span>
           </div>
         </div>
+      </div>
 
-        <button className="submitButton" type="submit">REVAISE</button>
-      </form>
+      <button className="submitButton" onClick={handleSubmit}>
+        REVAISE
+      </button>
     </div>
   )
 }
